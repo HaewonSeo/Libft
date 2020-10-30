2a45e4f74d9bab0d83f6698f10a00fb14760d238
HaewonSeo <haewon54@naver.com>
2020-10-30 16:42:11 +0900

    strlcpy

---
+++ libft/ft_strlcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: haseo <haseo@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/30 15:48:48 by haseo             #+#    #+#             */
+/*   Updated: 2020/10/30 16:41:31 by haseo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	char	*src2;
+	size_t	len_src;
+	size_t	i;
+
+
+
+	i = 0;
+	while (*src != '\0' && i < dstsize - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (len_src);
+}
