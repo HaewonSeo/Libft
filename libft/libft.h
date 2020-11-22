c8386e5880aaeaca9c310cfcf4dbbc3cb8875ad3
shw <shw2662@gamil.com>
2020-11-23 03:10:59 +0900

    norm

--- libft/libft.h
+++ libft/libft.h
@@ -1,3 +1,4 @@
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -16,11 +17,11 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-typedef struct	s_list
+typedef struct		s_list
 {
 	void			*content;
 	struct s_list	*next;
-}				t_list;
+}					t_list;
 
 /*
 **Part 1
