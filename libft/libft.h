8847a999779452131ae65dbe34777c7c8173939a
shw <shw2662@gamil.com>
2020-11-23 03:12:01 +0900

    norm

--- libft/libft.h
+++ libft/libft.h
@@ -1,4 +1,3 @@
-
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -17,15 +16,15 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-typedef struct		s_list
+typedef struct	s_list
 {
 	void			*content;
 	struct s_list	*next;
-}					t_list;
+}				t_list;
 
 /*
-**Part 1
-**Memory Functions
+** Part 1
+** Memory Functions
 */
 void			*ft_memset(void *dest, int c, size_t len);
 void			ft_bzero(void *b, size_t len);
@@ -36,7 +35,7 @@ void			*ft_memchr(const void *b, int c, size_t len);
 int				ft_memcmp(const void *b1, const void *b2, size_t len);
 
 /*
-**String Functions
+** String Functions
 */
 size_t			ft_strlen(const char *s);
 size_t			ft_strlcpy(char *dst, const char *src, size_t dstsize);
@@ -47,7 +46,7 @@ char			*ft_strnstr(const char *big, const char *little, size_t len);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
 
 /*
-**Utility Functions
+** Utility Functions
 */
 int				ft_atoi(const char *nptr);
 int				ft_isalpha(int c);
@@ -59,14 +58,14 @@ int				ft_toupper(int c);
 int				ft_tolower(int c);
 
 /*
-**Use malloc()
+** Use malloc()
 */
 void			*ft_calloc(size_t number, size_t size);
 char			*ft_strdup(const char *str);
 
 /*
-**Part 2
-**String Functions
+** Part 2
+** String Functions
 */
 char			*ft_substr(char const *s, unsigned int start, size_t len);
 char			*ft_strjoin(char const *s1, char const *s2);
@@ -76,7 +75,7 @@ char			*ft_itoa(int n);
 char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
 /*
-**Print Functions
+** Print Functions
 */
 void			ft_putchar_fd(char c, int fd);
 void			ft_putstr_fd(char *s, int fd);
@@ -84,7 +83,7 @@ void			ft_putendl_fd(char *s, int fd);
 void			ft_putnbr_fd(int n, int fd);
 
 /*
-**Bonus part
+** Bonus part
 */
 t_list			*ft_lstnew(void *content);
 void			ft_lstadd_front(t_list **lst, t_list *new);
@@ -94,6 +93,7 @@ void			ft_lstadd_back(t_list **lst, t_list *new);
 void			ft_lstdelone(t_list *lst, void (*del)(void *));
 void			ft_lstclear(t_list **lst, void (*del)(void *));
 void			ft_lstiter(t_list *lst, void (*f)(void *));
-t_list			*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list			*ft_lstmap(t_list *lst, void *(*f)(void *),
+							void (*del)(void *));
 
 #endif
